7f56a7ecb51fb72e42c534d79d15e3e07e57a56d
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-09-02 15:00:15 +0100

    More consistent API behaviour for LED with brightness

--- test/lib/test-led.js
+++ test/lib/test-led.js
@@ -23,15 +23,23 @@ describe('LED', function () {
   });
 
   describe('#on', function () {
-    it('turns LED on', function () {
+    it('turns LED on with default brightness', function () {
       var led = subject.create(9, { wpi: this.wpi });
       led.on();
       assert.ok( this.wpi.digitalWrite.calledWith(9, 1) );
+      assert.equal( led.brightness(), 100 );
     });
     it('sends correct signal when reversed', function () {
       var led = subject.create(9, { reverse:true, wpi: this.wpi });
       led.on();
       assert.ok( this.wpi.digitalWrite.calledWith(9, 0) );
+    });
+    it('uses pwm if brightness changed', function () {
+      var led = subject.create(9, { wpi: this.wpi });
+      led.brightness(10);
+      led.on();
+      assert.ok( this.wpi.softPwmWrite.calledOnce );
+      assert.ok( this.wpi.softPwmWrite.calledWith(9, 10) );
     });
     it('returns self for chaining', function () {
       var led = subject.create(9, { wpi: this.wpi });
@@ -50,6 +58,20 @@ describe('LED', function () {
       led.off();
       assert.ok( this.wpi.digitalWrite.calledWith(9, 1) );
     });
+    it('uses pwm if brightness changed', function () {
+      var led = subject.create(9, { wpi: this.wpi });
+      led.brightness(50);
+      led.off();
+      assert.ok( this.wpi.softPwmWrite.calledOnce );
+      assert.ok( this.wpi.softPwmWrite.calledWith(9, 0) );
+    });
+    it('uses reversed pwm if brightness changed', function () {
+      var led = subject.create(9, { reverse: true, wpi: this.wpi });
+      led.brightness(10);
+      led.off();
+      assert.ok( this.wpi.softPwmWrite.calledOnce );
+      assert.ok( this.wpi.softPwmWrite.calledWith(9, 100) );
+    });
     it('returns self for chaining', function () {
       var led = subject.create(9, { wpi: this.wpi });
       assert.equal( led.off(), led );
@@ -57,6 +79,10 @@ describe('LED', function () {
   });
 
   describe('#brightness', function () {
+    it('returns current value if no args', function () {
+      var led = subject.create(9, { wpi: this.wpi });
+      assert.equal( led.brightness(), 100 );
+    });
     it('sets pin for software PWM', function () {
       var pwmInitialValue = 0,
           pwmRange = 100,
@@ -64,7 +90,7 @@ describe('LED', function () {
 
       led.brightness(40);
       assert.ok( this.wpi.softPwmCreate.calledWith(9, pwmInitialValue, pwmRange) );
-      assert.ok( this.wpi.softPwmWrite.calledWith(9, 40) );
+      assert.notOk( this.wpi.softPwmWrite.called );
     });
     it('handles reversed LED', function () {
       var pwmInitialValue = 100,
@@ -73,7 +99,6 @@ describe('LED', function () {
 
       led.brightness(40);
       assert.ok( this.wpi.softPwmCreate.calledWith(9, pwmInitialValue, pwmRange) );
-      assert.ok( this.wpi.softPwmWrite.calledWith(9, 60) );
     });
     it('calls PWM setup only once', function () {
       var led = subject.create(9, { wpi: this.wpi });
